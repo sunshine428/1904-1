149c377f66e5369edc450f726ef1dd7612fc967b
sunshine428 <2484282331@qq.com>
2020-01-16 16:35:45 +0800

    first commit

--- resources/views/index/wechat_login.blade.php
+++ resources/views/index/wechat_login.blade.php
@@ -45,7 +45,7 @@
 <!-- 全局js -->
 {{--<script src="{{asset('hadmin/js/jquery.min.js?v=2.1.4')}}"></script>--}}
 {{--<script src="{{asset('hadmin/js/bootstrap.min.js?v=3.3.6')}}"></script>--}}
-<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('/js/jquery.min.js')}}"></script>
 
 </body>
 
