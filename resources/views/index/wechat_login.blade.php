db5778ba4b2c3f42589d03fbe2162df7c69a3557
sunshine428 <2484282331@qq.com>
2020-01-16 16:16:55 +0800

    first commit

--- resources/views/index/wechat_login.blade.php
+++ resources/views/index/wechat_login.blade.php
@@ -11,11 +11,11 @@
     <meta name="keywords" content="">
     <meta name="description" content="">
 
-    <link rel="shortcut icon" href="favicon.ico"> <link href="/hadmin/css/bootstrap.min.css?v=3.3.6" rel="stylesheet">
-    <link href="/hadmin/css/font-awesome.css?v=4.4.0" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico"> <link href="{{asset('hadmin/css/bootstrap.min.css?v=3.3.6')}}" rel="stylesheet">
+    <link href="{{asset('hadmin/css/font-awesome.css?v=4.4.0')}}" rel="stylesheet">
 
-    <link href="/hadmin/css/animate.css" rel="stylesheet">
-    <link href="/hadmin/css/style.css?v=4.1.0" rel="stylesheet">
+    <link href="{{asset('hadmin/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('hadmin/css/style.css?v=4.1.0')}}" rel="stylesheet">
     <!--[if lt IE 9]>
     <meta http-equiv="refresh" content="0;ie.html" />
     <![endif]-->
@@ -43,8 +43,8 @@
 </div>
 
 <!-- 全局js -->
-<script src="/hadmin/js/jquery.min.js?v=2.1.4"></script>
-<script src="/hadmin/js/bootstrap.min.js?v=3.3.6"></script>
+<script src="{{asset('hadmin/js/jquery.min.js?v=2.1.4')}}"></script>
+<script src="{{asset('hadmin/js/bootstrap.min.js?v=3.3.6')}}"></script>
 <script src="{{asset('js/jquery.min.js')}}"></script>
 
 </body>
